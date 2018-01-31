e57b11918ada9c674c83786d54a61a3e86fd5419
Tomasz Luczynski <luczynski.tomek@gmail.com>
2018-01-31 13:30:41 +0100

    updated contact email address

--- MATLAB/Optimal_d_0/optim_d_0.m
+++ MATLAB/Optimal_d_0/optim_d_0.m
@@ -22,8 +22,7 @@
 % (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %
-% Contact: Tomasz Łuczyński: t.luczynski@jacobs-university.de / luczynski.tomek@gmail.com
-%
+% Contact: robotics@jacobs-university.de
 
 function [diff] = optim_d_0(x)
 
